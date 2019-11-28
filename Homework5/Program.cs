4a6fa25f758eb4df901356f1e34bd7f2b806de2a
blue.hunde@gmail.com <blue.hunde@gmail.com>
2019-11-28 16:11:36 +0300

    Добавьте файлы проекта.

---
+++ Homework5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] files = Directory.GetFiles(@"C:\Users\Useress\source", "*.txt", SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; i++)
+            {
+                //string str = Path.GetFileName(files[i]);
+                FileStream fs = new FileStream("text.txt", FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs);
+                DateTime date = File.GetLastWriteTime(files[i]);
+                sw.WriteLine(files[i] + " was created " + date);
+                sw.Close();
+            }
+        }
+    }
+}
